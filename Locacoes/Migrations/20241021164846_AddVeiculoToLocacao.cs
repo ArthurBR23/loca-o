2eff66c3f7e9e05b19658c55b0efa762a23072e1
ArthurBR23 <arthursilvah736@gmail.com>
2024-10-28 15:44:56 -0300

    Adicionar arquivos de projeto.

---
+++ Locacoes/Migrations/20241021164846_AddVeiculoToLocacao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Locacoes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVeiculoToLocacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VeiculoId",
+                table: "Locacoes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VeiculoId",
+                table: "Locacoes");
+        }
+    }
+}
---
+++ Locacoes/Migrations/20241021170156_AddVeiculoLocacao.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Locacoes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVeiculoLocacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VeiculoId",
+                table: "Locacoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VeiculoIds",
+                table: "Locacoes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locacoes_VeiculoId",
+                table: "Locacoes",
+                column: "VeiculoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locacoes_Veiculo_VeiculoId",
+                table: "Locacoes",
+                column: "VeiculoId",
+                principalTable: "Veiculo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locacoes_Veiculo_VeiculoId",
+                table: "Locacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Locacoes_VeiculoId",
+                table: "Locacoes");
+
+            migrationBuilder.DropColumn(
+                name: "VeiculoIds",
+                table: "Locacoes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VeiculoId",
+                table: "Locacoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
